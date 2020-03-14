d870b2c79c427f02718a0944499105177fdd5c2b
dafaak <israel-03j@hotmail.com>
2020-03-14 11:24:14 -0500

    4 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import {TranslocoService} from "@ngneat/transloco";
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  constructor(private _translocoService:TranslocoService) { }
   name = 'Angular';
+   cambiarIdioma() {
+    const idiomas = this._translocoService.getAvailableLangs();
+    const idiomaActual = this._translocoService.getActiveLang();
+    let cambiarA: string;
+    idiomas.forEach(idioma => {
+      if (idioma !== idiomaActual) {
+        cambiarA = idioma;
+      }
+    })
+    this._translocoService.setActiveLang(`${cambiarA}`)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { TranslocoRootModule } from './transloco-root.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule,TranslocoRootModule,
+    HttpClientModule ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
